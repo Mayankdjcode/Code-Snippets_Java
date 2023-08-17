ed8fbcc90e9ed0c7cfa1cca684aafcf3079cda4b
Mayank Kumar Singh <77068048+Mayankdjcode@users.noreply.github.com>
2023-08-17 21:34:13 +0530

    Add files via upload

---
+++ IncDec3.java
@@ -0,0 +1,10 @@
+// Another example depicting simple arithmetic operation after incrementation and decrementation of a variable.
+
+class IncDec3 {
+    public static void main(String[] args) 
+	{
+		int a=7;
+		System.out.println((--a + --a)*(++a - a--)+(a++ + --a)*(++a + a--));  //120
+                        //  (6  +  5) *(6   - 6  )+(5   +   5)*(6   +  6)
+	}
+}

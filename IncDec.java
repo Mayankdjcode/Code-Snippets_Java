9d145410812f83efd0c70772a5abce3258248a61
Mayank Kumar Singh <77068048+Mayankdjcode@users.noreply.github.com>
2023-08-17 21:18:41 +0530

    Add files via upload

---
+++ IncDec.java
@@ -0,0 +1,21 @@
+//Code analyzing behaviour of Pre, Post Increment and Decrement operators
+
+class IncDec 
+{
+	public static void main(String[] args) 
+	{
+		
+		int a=7;
+		System.out.println(a);    //7
+		
+        //increment
+        System.out.println(a++);  //post-increment //7
+		System.out.println(++a);  //pre-increment  //9
+        
+        //decrement
+		System.out.println(a--);  //post-increment //9
+		System.out.println(--a);  //pre-increment  //7
+		  
+
+	}
+}

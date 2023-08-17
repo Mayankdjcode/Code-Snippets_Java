4aa8bd57c69c2026eaa195ffec9a3073a4a5750a
Mayank Kumar Singh <77068048+Mayankdjcode@users.noreply.github.com>
2023-08-17 22:37:20 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,18 @@
+// Bitwise Operations
+
+// These operations are performed on the Binary Level . 
+
+class Bitwise 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=2;
+		System.out.println(a&b);  // bitwise AND operator
+		System.out.println(a|b);  // bitwise OR
+		System.out.println(a^b);  // bitwise XOR
+		System.out.println(a<<b); // left shift operator (left shift a in binary form by b number of digits)
+		System.out.println(a>>b); // right shift operator (right shift a in binary form by b number of digits)
+		
+	}
+}

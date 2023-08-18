5ce013da5a5965ea1f9ebe5f424f8e9aca2ca2e9
Mayank Kumar Singh <77068048+Mayankdjcode@users.noreply.github.com>
2023-08-18 18:06:26 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,13 @@
+//Code to demonstrate the use and behavior of Ternary operator
+
+
+class Ternary 
+{
+	public static void main(String[] args) 
+	{
+		int x=5;
+		int y=3;
+		int max = (x > y) ? x : y; // Explanation: if (x > y) is true, x is printed. Else, y.
+		System.out.println(max);
+	}
+}

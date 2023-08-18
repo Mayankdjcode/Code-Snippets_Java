1b73cf062a364f92188c28175a7aee40cf255719
Mayank Kumar Singh <77068048+Mayankdjcode@users.noreply.github.com>
2023-08-18 18:14:21 +0530

    Add files via upload

---
+++ IncDec4.java
@@ -0,0 +1,29 @@
+// Incement and decrement operations with arithmetic operation.
+
+class IncDec4
+{
+	public static void main(String[] args) 
+	{
+		int a=6;
+		int b=++a*2+a++ + --a - a--;
+		System.out.println(b);
+
+		int x=6;
+		int y=x-- + --x + x++ - ++x;
+		System.out.println(y);	
+		
+		int q=3;
+		int q1=q-- + q++ - --q * ++q + q--;
+		System.out.println(q1);
+		
+		int l=10;
+		int l1=l-- * --l * l++ - l-- * l++;
+		System.out.println(l1);	
+
+		int g=4;
+		int g1=g-- / ++g * --g + g++ * g-- - --g / g++;
+		System.out.println(g1);	
+		
+
+	}
+}

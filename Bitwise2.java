a7d7455b11beb30d1ec8cf62abddd11208153b55
Mayank Kumar Singh <77068048+Mayankdjcode@users.noreply.github.com>
2023-08-18 18:01:52 +0530

    Add files via upload

---
+++ Bitwise2.java
@@ -0,0 +1,15 @@
+// Bitwise left-shift and right-shift operations with assignment.
+
+class Bitwise2
+{
+	public static void main(String[] args) 
+	{
+		int a=3;
+		int b=a << (2 + a) >> 1;   // 3<<5 >>1
+		System.out.println(b);      
+
+		int m=5;
+		int n=m<<(8-m)<<2;         // 5<<3 <<2
+		System.out.println(n);
+	}
+}

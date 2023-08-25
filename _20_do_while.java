56859b550f55c58eb6a5cfbded7b5eb2d7cd2086
Mayank Kumar Singh <77068048+Mayankdjcode@users.noreply.github.com>
2023-08-25 23:04:13 +0530

    Add files via upload

---
+++ _20_do_while.java
@@ -0,0 +1,15 @@
+// Use of Do While loop
+
+public class _20_do_while {
+
+    public static void main(String[] args) {
+
+        int i=5;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i<8);
+
+        System.out.println("Sab theek hai");
+    }
+}
